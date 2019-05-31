ddea3014e9c6487eba483a55319bfc1e27977d25
Habib <thiernohabibbah@gmail.com>
2019-05-31 18:33:18 +0200

    creation du client

--- BibliothequeService/src/main/java/com/opendevup/repository/PretRepository.java
+++
@@ -1,9 +0,0 @@
-package com.opendevup.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.opendevup.metier.Pret;
-
-public interface PretRepository extends JpaRepository<Pret, Long>{
-
-}
---
+++ ClientBibliotheque/src/main/java/com/opendevup/client/Livre.java
@@ -0,0 +1,152 @@
+
+package com.opendevup.client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour livre complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType name="livre">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="auteur" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="categorie" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="nombreExemplaire" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="pages" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="titre" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "livre", propOrder = {
+    "auteur",
+    "categorie",
+    "nombreExemplaire",
+    "pages",
+    "titre"
+})
+public class Livre {
+
+    protected String auteur;
+    protected String categorie;
+    protected int nombreExemplaire;
+    protected int pages;
+    protected String titre;
+
+    /**
+     * Obtient la valeur de la propriété auteur.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAuteur() {
+        return auteur;
+    }
+
+    /**
+     * Définit la valeur de la propriété auteur.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAuteur(String value) {
+        this.auteur = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété categorie.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCategorie() {
+        return categorie;
+    }
+
+    /**
+     * Définit la valeur de la propriété categorie.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCategorie(String value) {
+        this.categorie = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété nombreExemplaire.
+     * 
+     */
+    public int getNombreExemplaire() {
+        return nombreExemplaire;
+    }
+
+    /**
+     * Définit la valeur de la propriété nombreExemplaire.
+     * 
+     */
+    public void setNombreExemplaire(int value) {
+        this.nombreExemplaire = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété pages.
+     * 
+     */
+    public int getPages() {
+        return pages;
+    }
+
+    /**
+     * Définit la valeur de la propriété pages.
+     * 
+     */
+    public void setPages(int value) {
+        this.pages = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété titre.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getTitre() {
+        return titre;
+    }
+
+    /**
+     * Définit la valeur de la propriété titre.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTitre(String value) {
+        this.titre = value;
+    }
+
+}
